810742fe811b926f5e337e6c490e170af67d1aa5
CharlieCudi <koiicudi209@gmail.com>
2021-10-21 13:31:26 -0700

    Cleaned up code
    Cleaned up program and added comments to describe code

--- QuadraticFormula.java
+++ QuadraticFormula.java
@@ -1,39 +1,41 @@
 import java.util.Scanner;
+
 public class QuadraticFormula {
     public static void main(final String[] args) {
-
+            //Added scanner
         Scanner userInput = new Scanner (System.in);
         
+
+            //userInput for variable 'b'
         System.out.println("Please enter the value of b:");
         double valueB = userInput.nextDouble();
             
-            //Prints out value of 'b'
-            System.out.println(valueB);
-        
+
+            //userInput for variable 'a'
         System.out.println("Please enter the value of 'a'");
         double valueA = userInput.nextDouble();
 
-            //Prints out value of 'a'
-            System.out.println(valueA);
 
+            //userInput for variable 'c'
         System.out.println("Please enter the value of 'c");
         double valueC = userInput.nextDouble();
 
-            //Prints out value of 'c'
-            System.out.println(valueC);
 
+            //First step, figure the squareroot of 'b*b - 4ac'
     double squareRt = (Math.sqrt((valueB)*(valueB) - 4*(valueA*valueC)));
 
-        //Result of the square root portion
-        System.out.println(squareRt);
-    
+
+            //Second step, subract and add '-b' from the result of the square root
     double negBPlus = (-(valueB) - squareRt);
     double negAPlus = (-(valueB) + squareRt);
 
+            //Third step, divide the result with '2*a'
     double answer1 = (negBPlus / 2*(valueA));
     double answer2 = (negAPlus / 2*(valueA));
 
-    System.out.println("The answer is: " + "(" + (answer1) + "," + (answer2) + ")");
+
+            //Prints out the answer of the equation
+    System.out.println("The answer is: " + "(" + (answer1) + ", " + (answer2) + ")");
     
     
 
